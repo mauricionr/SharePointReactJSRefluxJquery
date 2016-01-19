442cf9d4d71990e067891993eace8d1e6ffe55b3
Mauricio N. Reis <mauricioskateboard@gmail.com>
2016-01-19 18:21:36 -0200

    CRUD SharePoint

---
+++ src/components/contact/ContactItem.js
@@ -0,0 +1,64 @@
+var ContactItem = React.createClass({
+  propTypes: {
+    Title: React.PropTypes.string,
+    Description: React.PropTypes.string,
+    Email: React.PropTypes.string,
+  },
+  getInitialState:function(){
+  	return {
+  		isEditing:false,
+  		obj:{}
+  	}
+  },
+  deleteItem:function(){
+  	ContactsStore.onDeleteItem(this.props)
+  },
+  editItem:function(){
+  	this.setState({
+  		isEditing:true
+  	})
+  },
+  handleChange:function(Field,event){
+  	 this.state.obj[Field] = event.target.value
+  	 this.state.obj.Id = this.props.Id
+  	 this.setState({
+  	 	obj: this.state.obj,
+  	 	isEditing:true
+  	 });
+  },
+  saveItem:function(){
+  	ContactsStore.onUpdateList(this.state.obj)
+	 this.setState({
+  		isEditing:false
+  	})
+  },
+  render: function() {
+  	var element;
+  	if(!this.state.isEditing){
+  		element = (
+	      React.createElement('div',{className: 'ContactItem'},
+	        React.createElement('div', {className: 'ContactItem-hold'},
+	        	React.createElement('p',{className:'Contact-Title'},this.props.Title),
+	        	React.createElement('p',{className:'Contact-Descricao'},this.props.Description),
+	        	React.createElement('p',{className:'Contact-Email'},this.props.Email),
+	        	React.createElement('button',{className:'Contact-Delete',type:'button',onClick:this.deleteItem},'Deletar'),
+	        	React.createElement('button',{className:'Contact-Edit',type:'button',onClick:this.editItem},'Editar')
+	        )
+	      )
+	    )
+  	}else{
+  		element = (
+	      React.createElement('div',{className: 'ContactItem'},
+	        React.createElement('div', {className: 'ContactItem-hold'},
+	        	React.createElement('input',{className:'Contact-Title',type:'Text',defaultValue:this.props.Title,onChange:this.handleChange.bind(null,'Title')}),
+	        	React.createElement('input',{className:'Contact-Descricao',type:'Text',defaultValue:this.props.Description,onChange:this.handleChange.bind(null,'Description')}),
+	        	React.createElement('input',{className:'Contact-Email',type:'Text',defaultValue:this.props.Email,onChange:this.handleChange.bind(null,'Email')}),
+	        	React.createElement('button',{className:'Contact-Delete',type:'button',onClick:this.deleteItem},'Deletar'),
+	        	React.createElement('button',{className:'Contact-SaveItem',type:'button',onClick:this.saveItem},'Salvar')
+	        )
+	      )
+	    )
+  	}
+    return element
+  },
+});
---
+++ src/stores/spcontactsstores.js
@@ -0,0 +1,91 @@
+﻿var ContactsStore = Reflux.createStore({
+    listenables: [ContactsActions],
+    contactslist: [],
+    listTitle:'Contacts',
+    InternalListName:'Contacts',
+    OdataQuery:"",
+    absoluteUrl:_spPageContextInfo.webAbsoluteUrl,
+    sourceUrl: function(id){
+    	if(id){
+    		return this.absoluteUrl+"/_api/web/lists/getbytitle('"+this.listTitle+"')/items("+id+")";
+    	}else{
+    		return this.absoluteUrl+"/_api/web/lists/getbytitle('"+this.listTitle+"')/items?"+this.OdataQuery;
+    	}
+    },
+    init: function() {
+    	this.onFetchList();
+    },
+    onFetchList: function() {
+        $.ajax({
+            url: this.sourceUrl(),
+            dataType: 'json',
+            cache: false,
+            context: this,
+            success: function(data) {
+                this.contactslist = data.value;
+                this.trigger(this.contactslist);
+            },
+            error: function (data) {
+	            console.log(JSON.stringify(data));
+	        }
+        });
+    },
+   onCreateItem:function(object) {
+	    var item = $.extend({"__metadata": { "type": "SP.Data."+this.InternalListName+"ListItem"}}, object);
+	    $.ajax({
+	        url: this.sourceUrl(),
+	        method: "POST",
+	        contentType: "application/json;odata=verbose",
+	        data: JSON.stringify(item),
+	        headers: {"Accept": "application/json;odata=verbose","X-RequestDigest": $("#__REQUESTDIGEST").val()},
+	        success: function (data) {
+	            this.onFetchList()
+	        }.bind(this),
+	        error: function (data) {
+	            console.log(data.responseJSON.error.message.value);
+	        }
+	    });
+	},
+   onDeleteItem:function(object){
+	   	var item = $.extend({"__metadata": { "type": "SP.Data."+this.InternalListName+"ListItem"}}, object);
+	    $.ajax({
+	        url: this.sourceUrl(object.Id),
+	        method: "POST",
+	        contentType: "application/json;odata=verbose",
+	        data: JSON.stringify(item),
+	        headers: {
+		        "IF-MATCH": "*",
+	        	"Accept": "application/json;odata=verbose",
+	        	"X-RequestDigest": $("#__REQUESTDIGEST").val(),
+	        	'X-HTTP-Method':"DELETE",
+	        },
+	        success: function (data) {
+	            this.onFetchList()
+	        }.bind(this),
+	        error: function (data) {
+	            console.log(data.responseJSON.error.message.value);
+	        }
+	    });
+   },
+   onUpdateList:function(object){
+   		var item = $.extend({"__metadata": { "type": "SP.Data."+this.InternalListName+"ListItem"}}, object);
+	    $.ajax({
+	        url: this.sourceUrl(object.Id),
+	        method: "POST",
+	        contentType: "application/json;odata=verbose",
+	        data: JSON.stringify(item),
+	        headers: {
+		        "IF-MATCH": "*",
+	        	"Accept": "application/json;odata=verbose",
+	        	"X-RequestDigest": $("#__REQUESTDIGEST").val(),
+	        	'X-HTTP-Method':"MERGE",
+	        },
+	        success: function (data) {
+	            this.onFetchList()
+	        }.bind(this),
+	        error: function (data) {
+	            console.log(data.responseJSON.error.message.value);
+	        }
+	    });
+   }
+});
